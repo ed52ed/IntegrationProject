12ee5a5e81ddc2d3f6b501633984e51b014893b1
ed52ed <98991483+ed52ed@users.noreply.github.com>
2025-01-29 04:27:25 -0800

    Update pytest_script.py

--- pytest_script.py
+++ pytest_script.py
@@ -1,6 +1,6 @@
 import requests
 
 def test_homepage():
-    response = requests.get("http://localhost:5000")
+    response = requests.get("http://localhost:8080")
     assert response.status_code == 200
     assert "Home Page" in response.text
